4838c6fb4acef9d3bddf9a5337826dc659ab3127
John Long <jlcomp45@gmail.com>
2019-12-10 11:44:10 -0600

    completed login and log out - and can register new user with post.  still want to correct data structure and use helper function to ensure user entry is consistent

--- src/components/Cue.js
+++ src/components/Cue.js
@@ -3,11 +3,50 @@ import NavBar from './nav/NavBar'
 import ApplicationViews from './ApplicationViews'
 
 class Cue extends Component {
+
+    state = {
+        user: false,
+        userId: ""
+    }
+    
+    isAuthenticated = () => localStorage.getItem("credentials") !== null
+    
+    setUser = authObj => {
+        localStorage.setItem(
+            "credentials",
+            JSON.stringify(authObj)
+        )
+        this.setState({
+            user: this.isAuthenticated(),
+        });
+    }
+
+    getUser = JSON.parse(localStorage.getItem("credentials"))
+
+    clearUser = () => {
+        localStorage.removeItem("credentials")
+
+        this.setState({
+            user: this.isAuthenticated()
+        });
+    }
+
+    componentDidMount() {
+        this.setState({
+            user: this.isAuthenticated()
+        })
+    }
+
     render() {
+        console.log("cue.js state", this.state)
         return (
             <>
-                <NavBar />
-                <ApplicationViews />
+                <NavBar user={this.state.user}
+                clearUser={this.clearUser} />
+                <ApplicationViews user={this.state.user}
+                setUser={this.setUser}
+                searchUsers={this.searchUsers}
+                getUser={this.getUser} />
             </>
 
         );
--- src/modules/APIManager.js
+++ src/modules/APIManager.js
@@ -1,10 +1,24 @@
 const remoteURL = "http://localhost:5002"
 
 export default {
-  get(id) {
-    return fetch(`${remoteURL}/cues/${id}/?_expand=builder&_expand=wrap&_expand=style`).then(result => result.json())
-  },
-  getAll() {
-    return fetch(`${remoteURL}/cues/?_expand=builder&_expand=wrap&_expand=style`).then(result => result.json())
-  }
+    get(id) {
+        return fetch(`${remoteURL}/cues/${id}/?_expand=builder&_expand=wrap&_expand=style`).then(result => result.json())
+    },
+    getAll() {
+        return fetch(`${remoteURL}/cues/?_expand=builder&_expand=wrap&_expand=style`).then(result => result.json())
+    },
+    post(items, newItem) {
+        return fetch(`${remoteURL}/${items}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newItem)
+        }).then(data => data.json())
+    },
+    //   fetch call to get users
+    searchUser: (searchAll) => {
+        return fetch(`${remoteURL}/users?&q=${searchAll}`)
+            .then(users => users.json())
+    },
 }
